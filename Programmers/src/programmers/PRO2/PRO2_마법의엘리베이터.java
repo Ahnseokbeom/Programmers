51d2da691705bb06de4f9e900f0b4ee0f8dd4103
seobkeom <ajm153123@naver.com>
2023-11-25 17:42:46 +0900

    PRO2 마법의엘리베이터

---
+++ Programmers/src/programmers/PRO2/PRO2_마법의엘리베이터.java
@@ -0,0 +1,31 @@
+package programmers.PRO2;
+
+public class PRO2_마법의엘리베이터 {
+	public static void main(String[] args) {
+		System.out.println(solution(16));
+		System.out.println(solution(2554));
+		System.out.println(solution(100000000));
+	}
+	public static int solution(int storey) {
+        int answer = 0;
+        while (storey != 0) {
+            int num = storey % 10;
+
+            if (num > 5) {
+                answer += (10 - num);
+                storey += 10;
+            } else if (num < 5) {
+                answer += num;
+            } else {
+                if ((storey / 10) % 10 > 4) {
+                    storey += 10;
+                }
+                answer += num;
+            }
+
+            storey /= 10;
+        }
+
+        return answer;
+    }
+}
